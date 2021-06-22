f6abf3a07bb3748fd4f4ad2b6dccf93b6a3ab353
dhanuvanth <dhanuvanth@gmail.com>
2021-06-22 13:37:35 +0530

    Faster live streaming

--- Live_stream_seg.py
+++ Live_stream_seg.py
@@ -5,7 +5,7 @@ import cv2
 # Access camera
 capture = cv2.VideoCapture(0)
 
-segment_video = instance_segmentation()
+segment_video = instance_segmentation(infer_speed='rapid') # average, fast, rapid
 segment_video.load_model("mask_rcnn_coco.h5")
 segment_video.process_camera(capture, show_bboxes=True, frames_per_second= 10, output_video_name="output_streamq.mp4", show_frames= True,
 frame_name= "frame")
